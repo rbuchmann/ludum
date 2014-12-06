d368623dd04258fa81837df4a3127f7e26dfd48e
hut <hut@lepus.uberspace.de>
2014-12-06 17:40:00 +0100

    cursor keys now change the player velocity

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,4 +1,6 @@
 
+var velocity = 0;
+var cursors;
 
 function preload() {
 }
@@ -7,11 +9,23 @@ function create() {
 }
 
 function update() {
+    if (cursors.left.isDown) {
+        velocity = -100;
+    }
+    else if (cursors.right.isDown) {
+        velocity = 100;
+    }
+    else {
+        velocity = 0;
+    }
 }
 
 module.exports = {
   create: function(){
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    cursors = game.input.keyboard.createCursorKeys();
+
     return game;  
   }
 }
