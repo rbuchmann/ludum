730bdd0fe5107cef38bff9e54c4454f578ebb8ab
hut <hut@lepus.uberspace.de>
2014-12-06 19:12:34 +0100

    draw cursor sprite at the mouse pointer

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -2,17 +2,22 @@ var velocity = 0;
 var cursors;
 var playerAlive = false;
 var game;
+var mouse;
 
 function preload() {
   game.load.image('zombee', 'assets/zombee.jpg');
+  game.load.image('cursor1', 'images/cursor1.png');
+  game.load.image('cursor2', 'images/cursor2.png');
 }
 
 function create() {
   cursors = game.input.keyboard.createCursorKeys();
+  mouse = game.input.mousePointer;
   playerAlive = true;
 }
 
 function update() {
+  game.add.sprite(mouse.x - 16, mouse.y - 16, 'cursor1');
   if (playerAlive) {
     if (cursors.left.isDown) {
       velocity = -100;
